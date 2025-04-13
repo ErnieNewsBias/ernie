1cdca666689feb4fb7ca03718dd0c44e320edcd2
Raj Khettry <raj.khettry@gmail.com>
2025-04-12 22:17:14 -0700

    added port 3000 to cors

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 # Configure CORS to allow requests from multiple origins
 CORS(app, resources={r"/scrape": {"origins": [
+    "http://localhost:3000"
     "http://localhost:3001", 
     "https://ernienews-1031077341247.us-central1.run.app"
 ]}}) 
